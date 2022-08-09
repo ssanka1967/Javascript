288d9018b360c457349d1d4939e4b23b63d992c7
Srinivas Sanka <srinivas.sanka@gmilc.com>
2022-08-08 17:59:22 -0700

    Calc program

---
+++ Calc.js
@@ -0,0 +1,26 @@
+function add(x,y)
+{
+    z = x + y
+    return z
+}
+
+
+
+function subtract(a,b)
+{
+    c = a - b
+    return c
+}
+
+
+
+
+
+sum = add(2,3)
+console.log(sum)
+
+sub = subtract(4,9)
+console.log(sub)
+
+test = add(sum,sub)
+console.log(test)
